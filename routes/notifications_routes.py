40145dd50f0a5d7bc76eb44121b4b743ae906392
ArturGZ <arturo.quintana.016@gmail.com>
2023-12-06 21:39:26 -0600

    feat: add info to read all and update notification

--- routes/notifications_routes.py
+++ routes/notifications_routes.py
@@ -14,9 +14,9 @@ class NotifRoutes(Blueprint):
         self.route('/apinotif/notif', methods=['GET'])(self.get_task_list)
 
         """ ids: task-list, task, notification """
-        self.route('/apinotif/notif/<int:list_id>/<int:task_id>/<int:notif_id>', methods=['GET'])(self.get_notif_by_id)
+        self.route('/apinotif/notif/<int:list_id>/<int:task_id>', methods=['GET'])(self.get_notif_by_id)
         
-        self.route('/apinotif/notif/<int:list_id>/<int:task_id>/<int:notif_id>', methods=['PUT'])(self.update_due_notif)
+        self.route('/apinotif/notif/<int:list_id>/<int:task_id>', methods=['PUT'])(self.update_due_notif)
 
         """ self.route('/apinotif/notif', methods=['POST'])(self.add_notif)
 
@@ -30,9 +30,9 @@ class NotifRoutes(Blueprint):
             log.exception(f'Error fetching data from the database: {e}')
             return jsonify({'error': 'Failed to fetch data from the database'}), 500
     
-    def get_notif_by_id(self, list_id, task_id, notif_id):
+    def get_notif_by_id(self, list_id, task_id):
         try:
-            self.notif = self.notif_service.get_notif_by_id(list_id, task_id, notif_id)
+            self.notif = self.notif_service.get_notif_by_id(list_id, task_id)
             if self.notif:
                 return jsonify(self.notif), 200
             else: 
@@ -41,16 +41,22 @@ class NotifRoutes(Blueprint):
             log.exception(f'Error fetching notification by id from the database: {e}')
             return jsonify({'error': 'Failed to fetch notification by id from the database'}), 500
         
-    def update_due_notif(self, list_id, task_id, notif_id):
+    def update_due_notif(self, list_id, task_id):
         try:
             self.data=request.json
+            self.data = request.json
             log.info(self.data)
             if not self.data:
-                    return jsonify({'error': 'Invalid data'}), 400
-
-            self.due_notif = self.data.get('due_notif')
-
-            self.notif = self.notif_service.update_due_notif(list_id, task_id, notif_id, self.due_notif)
+                return jsonify({'error': 'Invalid data'}), 400
+            
+            self.due_notif = self.data.get('due')
+            
+            try:
+                self.notif_schema.validate_due(self.due_notif)
+            except ValidationError as e:
+                return(jsonify({'error': 'Invalid data date', 'details': e.messages}), 400)
+            
+            self.notif = self.notif_service.update_due_notif(list_id, task_id, self.due_notif)
             if self.notif:
                 return jsonify(self.notif), 200
             else: 
@@ -58,64 +64,3 @@ class NotifRoutes(Blueprint):
         except Exception as e:
             log.exception(f'Error updating due notification: {e}')
             return jsonify({'error': 'Error updating due notification'}), 500
-        
-    """ def add_notif(self):
-        try:
-            self.data = request.json
-            if not self.data:
-                return jsonify({'error': 'Invalid data'}), 400
-            
-            self.title = self.data.get('title')
-            self.author = self.data.get('author')
-
-            try:
-                self.notif_schema.validate_title(self.title)
-                self.notif_schema.validate_author(self.author)
-            except ValidationError as e:
-                 return(jsonify({'error': 'Invalid data', 'details': e.messages}), 400)
-            
-            self.new_notif = {
-                'title': self.title,
-                'author': self.author
-            }
-
-            self.created_notif = self.notif_service.add_notif(self.new_notif)
-            return jsonify(self.created_notif), 201
-        except Exception as e:
-            log.critical(f'Error adding a new notification to the database: {e}')
-
-    def update_notif(self, notif_id):
-        try:
-            self.data = request.json
-            if not self.data:
-                return jsonify({'error': 'Invalid data'}), 400
-            
-            self.title = self.data.get('title')
-            self.author = self.data.get('author')
-
-            try:
-                self.notif_schema.validate_title(self.title)
-                self.notif_schema.validate_author(self.author)
-            except ValidationError as e:
-                 return(jsonify({'error': 'Invalid data', 'details': e.messages}), 400)
-            
-            self.notif_updated = self.notif_service.update_notif(notif_id, self.data)
-
-            if self.notif_updated:
-                return jsonify(self.notif_updated), 200
-            else:
-                return jsonify({'error': 'notification not found'}), 404
-
-        except Exception as e:
-            log.critical(f'Error updating the notification in the database: {e}')
-
-    def delete_notif(self, notif_id):
-        try:
-            self.notif_deleted = self.notif_service.delete_notif(notif_id)
-            if self.notif_deleted:
-                return jsonify(self.notif_deleted), 200
-            else:
-                return jsonify({'error': 'notification not found'}), 404
-        except Exception as e:
-            log.critical(f'Error deleting the notification in the database: {e}')
- """
--- services/notifications_services.py
+++ services/notifications_services.py
@@ -8,57 +8,59 @@ class NotifService:
 
 
     def get_all_due_tasks(self):
-        try:
-            """ 0 to hide and 1 to show attribute """
-            self.tasks = list(self.db_connector.db.tasks.find({},{
-                '_id': 0, 
-                'tasks.name': 1,
-                'tasks.due': 1, 
-                'tasks.notifications': 1
-                }))
+        try:           
+            self.tasks = list(self.db_connector.db.tasks.find())
             
             self.due_tasks = list()
-            for list_tasks in self.tasks:
-                for task in list_tasks.values():
-                    for due_task in task:
-                        self.due_tasks.append(due_task)
-
+            for list_t in self.tasks:
+                for task in list_t['tasks']:
+                    if task['status'] == 'Pending':
+                        self.due_tasks.append({
+                            'list_id': list_t['_id'],
+                            'task_id': task['_id'],
+                            'name': task['name'],
+                            'due': task['due'],
+                            'status': task['status']
+                        })
             self.due_tasks = sorted(self.due_tasks, key=lambda task:datetime.strptime(task['due'],"%Y-%m-%d %H:%M:%S"))
+            
             return self.due_tasks
         except Exception as e:
             log.critical(f'Error fetching all tasks from the database: {e}')
             return jsonify({'error': f'Error fetching all tasks from the database: {e}'}), 500
         
     
-    def get_notif_by_id(self, list_id, task_id, notif_id):
+    def get_notif_by_id(self, list_id, task_id):
         try:
             self.notif = self.db_connector.db.tasks.find_one({
                 '_id': list_id,
-                'tasks._id': task_id,
-                'tasks.notifications._id': notif_id
+                'tasks._id': task_id
                 })
             
-            """ for task in self.notif['tasks']:
+            self.due_task = list()
+            for task in self.notif['tasks']:
                 if task['_id'] == task_id:
-                    for notif_task in task['notifications']:
-                        if notif_task['_id'] == notif_id:
-                            return list(notif_task) """
-    
-            return self.notif
+                    self.due_task.append({
+                        'list_id': list_id,
+                        'task_id': task_id,
+                        'name': task['name'],
+                        'due': task['due'],
+                        'status': task['status']
+                    })
+            return self.due_task
         except Exception as e:
             log.critical(f'Error fetching the notification id from the database: {e}')
             return jsonify({'error': f'Error fetching the notification id from the database: {e}'}), 500
 
-    def update_due_notif(self, list_id, task_id, notif_id, updated_due):
+    def update_due_notif(self, list_id, task_id, updated_due):
         try:
-            updated_notif = self.get_notif_by_id(list_id, task_id, notif_id)
+            updated_notif = self.get_notif_by_id(list_id, task_id)
             if updated_notif:
                 result = self.db_connector.db.tasks.update_one({
                 '_id': list_id,
                 'tasks._id': task_id,
-                'tasks.notifications._id': notif_id
                 },
-                {'$set': {'tasks.notifications.$.due_notif': updated_due}})
+                {'$set': {'tasks.$.due': updated_due}})
                 if result.modified_count > 0:
                     return updated_notif
                 else:
